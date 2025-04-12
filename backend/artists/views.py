1c86d9a175138bb1dbfe20f4632560d4ebf37392
Elbin12 <elbinshiby14@gmail.com>
2025-04-12 09:20:56 +0530

    Backed search view update

--- backend/artists/views.py
+++ backend/artists/views.py
@@ -14,21 +14,39 @@ from rest_framework.pagination import PageNumberPagination
 
 # Create your views here.
 
+def get_initials(name):
+    return ''.join([word[0] for word in name.split() if word]).lower()
+
+
 class SearchArtists(APIView):
     authentication_classes = []
     permission_classes = [permissions.AllowAny]
 
     def get(self, request):
         query = request.query_params.get('query')
+        query_lower = query.lower()
         if not query:
             return Response({'error':'Search key is required'},status=400)
-        artists = Artists.objects.annotate(
+        artist_qs = Artists.objects.annotate(
             similarity=TrigramSimilarity('name', query)
         ).filter(
-            Q(name__icontains=query) | Q(similarity__gt=0.3)
-        ).order_by('-similarity', '-popularity')[:10]
-        serilaizer = ArtistSerializer(artists, many=True)
-        return Response(serilaizer.data, status=200)
+            Q(name__icontains=query) | Q(similarity__gt=0.2)
+        ).order_by('-similarity', '-popularity')[:100]
+
+        initials_matches = []
+        for artist in Artists.objects.all():
+            if get_initials(artist.name) == query_lower:
+                initials_matches.append(artist)
+
+        all_matches = list({a.id: a for a in list(artist_qs) + initials_matches}.values())
+
+        all_matches_sorted = sorted(all_matches, key=lambda a: a.popularity, reverse=True)
+
+        # 5. Limit to top 10
+        top_10 = all_matches_sorted[:10]
+
+        serializer = ArtistSerializer(top_10, many=True)
+        return Response(serializer.data, status=200)
     
 class ArtistPagination(PageNumberPagination):
     page_size = 20
